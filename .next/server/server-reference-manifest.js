e5c7907852badb923ddaaa780322fbdcd6bc8f82
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2024-12-20 01:32:38 +0530

    version update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pA4GTwGwgSkK2zIYHatq+6726JxxAxUWyp8TVuOYSos=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
